bbeeced2cd73ba9b94007b2d083bfc3ca81ddf3b
Agustí Pou Company <agustipcom@gmail.com>
2024-07-13 11:26:43 +0200

    Update page to see the result

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import ItemForm from './components/ItemForm'
+import ItemList from './components/ItemList'
+import { ItemsProvider } from './context/ItemsContext'
+
 export default function Home () {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Technical Test</h1>
-    </main>
+    <ItemsProvider>
+      <main className="flex flex-col min-h-screen items-center p-24">
+        <div className='mb-8 flex flex-col items-center'>
+          <h1 className='text-3xl'>TECHNICAL TEST</h1>
+          <h2 className='text-xl'>CRUD - NextJS form</h2>
+        </div>
+        <ItemForm/>
+        <ItemList/>
+      </main>
+    </ItemsProvider>
   )
 }
