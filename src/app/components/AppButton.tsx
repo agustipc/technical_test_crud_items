4968d2d479fc3d5ace9071007c32836e99eab5ec
Agustí Pou Company <agustipcom@gmail.com>
2024-07-13 11:24:17 +0200

    Create button component

---
+++ src/app/components/AppButton.tsx
@@ -0,0 +1,10 @@
+export default function AppButton ({ title }: { title: string }) {
+  return (
+    <button
+        type="submit"
+        className="mt-6 w-full py-2 rounded-md text-white bg-teal-500 hover:bg-teal-700 transition-colors focus:outline-none focus:ring-1 focus:ring-offset-[1.5px] focus:ring-teal-500"
+      >
+        {title}
+      </button>
+  )
+}
