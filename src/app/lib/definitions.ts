c3d484ba5b0e2517a9dbfae9b39e71e6b4b304fd
Agustí Pou Company <agustipcom@gmail.com>
2024-07-13 11:24:54 +0200

    Create the types

---
+++ src/app/lib/definitions.ts
@@ -0,0 +1,17 @@
+export interface Item {
+  title: string
+  description: string
+}
+
+export interface ItemErrors {
+  title: string | null
+  description: string | null
+}
+
+export interface ItemsContextType {
+  items: Item[]
+  addItem: (newItem: Item) => void
+  removeItem: (index: number) => void
+  selectedItemIndex: number | null
+  selectItem: (intex: number) => void
+}
