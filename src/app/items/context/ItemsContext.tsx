6019fcc37589aa011dc2ad7e22fec94ed936848d
Agustí Pou Company <agustipcom@gmail.com>
2024-07-17 11:48:12 +0200

    Add items to localstorage

--- src/app/items/context/ItemsContext.tsx
+++ src/app/items/context/ItemsContext.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import { createContext, useState, useContext, useCallback } from 'react'
+import { createContext, useState, useContext, useCallback, useEffect } from 'react'
 import { type Item, type ItemsContextType } from '../../lib/definitions'
+import { LOCAL_STORAGE_ITEMS_KEY } from '@/app/lib/constants'
 
 // Create a default value for the context
 const defaultValue: ItemsContextType = {
@@ -24,16 +25,36 @@ export const ItemsProvider = ({ children }: { children: React.ReactNode }) => {
   const [items, setItems] = useState<Item[]>([])
   const [selectedItemIndex, setSelectedItemIndex] = useState<number | null>(null)
 
+  useEffect(() => {
+    const savedItems = localStorage.getItem(LOCAL_STORAGE_ITEMS_KEY)
+    if (savedItems !== null) {
+      setItems(JSON.parse(savedItems) as Item[])
+    }
+  }, [])
+
   // Create the functions to add an item, that will add a new list Item object
   // Here we use the useCallback hook to memoize the function and avoid unnecessary re-renders
   const addItem = useCallback((newItem: Item) => {
-    setItems((prevItems) => [...prevItems, newItem])
+    setItems((prevItems) => {
+      const updatedItems = [...prevItems, newItem]
+      saveItemsToLocalStorage(updatedItems)
+      return updatedItems
+    })
   }, [])
+
+  // Save the items to localStorage
+  const saveItemsToLocalStorage = (items: Item[]) => {
+    localStorage.setItem(LOCAL_STORAGE_ITEMS_KEY, JSON.stringify(items))
+  }
 
   // Create the function to remove an item, that will remove the item from the list and update the selected item index
   // Passing the selectedItemIndex as a dependency in this case is necessary to update the function accordingly to the selected item
   const removeItem = useCallback((index: number) => {
-    setItems((prevItems) => prevItems.filter((_, i) => i !== index))
+    setItems((prevItems) => {
+      const updatedItems = prevItems.filter((_, i) => i !== index)
+      saveItemsToLocalStorage(updatedItems)
+      return updatedItems
+    })
 
     // If the selectedItemIndex is equal to the on that's being remove its turned to null
     // If the selectedItemIndex is greater than the one that's being remove it's decremented by 1 to match the new position
